199b9ab57b78c958d0c0984589cde9c8b2f3ba52
Korben Zhang <korbenzhang@gmail.com>
2015-01-26 15:45:30 +0800

    add a bin

---
+++ bin/colne_bundles.sh
@@ -0,0 +1,15 @@
+
+
+cd ~/.vim/bundle/ && \
+	git clone https://github.com/tomtom/tlib_vim.git
+
+
+cd ~/.vim/bundle/ && \
+	git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+
+cd ~/.vim/bundle/ && \
+	git clone https://github.com/garbas/vim-snipmate.git
+
+
+cd ~/.vim/bundle/ && \
+	git clone https://github.com/honza/vim-snippets.git
