6aadbd5fb90a25ed6af66ba5c9d82736ff41c72a
korbenzhang <korbenzhang@gmail.com>
2015-01-26 18:21:16 +0800

    update vimrc

--- bin/clone_bundles.sh
+++ bin/clone_bundles.sh
@@ -1,15 +1,15 @@
+#!/bin/bash
+
+cd ~/.vim/bundle
+
+git clone https://github.com/tomtom/tlib_vim.git
+
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+
+git clone https://github.com/garbas/vim-snipmate.git
+
+git clone https://github.com/honza/vim-snippets.git
+
+cd -
 
 
-cd ~/.vim/bundle/ && \
-	git clone https://github.com/tomtom/tlib_vim.git
-
-
-cd ~/.vim/bundle/ && \
-	git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
-
-cd ~/.vim/bundle/ && \
-	git clone https://github.com/garbas/vim-snipmate.git
-
-
-cd ~/.vim/bundle/ && \
-	git clone https://github.com/honza/vim-snippets.git
