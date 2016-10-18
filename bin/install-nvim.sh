9b6eb6b792b7d9401d2bab72431467d82b678e23
Korben Zhang <korbenzhang@gmail.com>
2016-10-19 01:12:58 +0800

    fix

---
+++ bin/install-nvim.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
+# depends
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+
---
+++ bin/update-alternative-nvim.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
+
+
