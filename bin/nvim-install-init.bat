1b5569e030fcbe55be920b14974e3ce50b0cc7d3
Korben Zhang <korbenzhang@gmail.com>
2017-07-13 19:24:38 +0800

    fix

---
+++ bin/nvim-install-init.bat
@@ -0,0 +1,11 @@
+
+REM 
+REM install nvim init file
+REM REM c:\Users\zsc\AppData\Local\nvim
+REM 
+
+mkdir %USERPROFILE%\AppData\Local\nvim
+
+copy %HOME%\.vim\init.vim %USERPROFILE%\AppData\Local\nvim\ 
+
+
