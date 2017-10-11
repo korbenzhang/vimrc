778c467ed5e4ac8fd86ab0dc9a0ae212d4f0c2ae
Korben Zhang <korbenzhang@gmail.com>
2017-10-11 17:25:26 +0800

    fix

---
+++ bin/mingw_make.bat
@@ -0,0 +1,3 @@
+
+
+cd vim\src && mingw32-make.exe -f Make_ming.mak PYTHON=D:\apps\Python27 PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON3=D:\apps\Python36 PYTHON3_VER=36 DYNAMIC_PYTHON3=yes GUI=yes IME=yes
---
+++ bin/win_make.bat
@@ -0,0 +1,6 @@
+
+REM build vim VC++
+
+nmake.exe -f Make_mvc.mak PYTHON=d:\apps\Python27 PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON3=D:\apps\Python36 PYTHON3_VER=36 DYNAMIC_PYTHON3=yes GUI=yes IME=yes
+
+
